f10572394aeedbc9946ab075d282b3b668e0e075
INDHU1402 <indhushivanip2001@gmail.com>
2022-01-11 20:13:55 +0530

    added preview while inserting image

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
 import cv2
 import os
+from flask.helpers import send_file
 import numpy as np
 from datetime import datetime
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-
+global pth
 @app.route("/")
 def home():
     flg = True
@@ -50,9 +51,9 @@ def register():
 @app.route('/delete', methods=["GET", "POST"])
 def delete():
     fn = request.form["file"]
-    file_name = os.path.join('/home/indhu/Desktop/AttendanceSystem/images/', secure_filename(fn))
+    file_name = os.path.join('/home/indhu/Desktop/AttendanceSystem/ss6/', secure_filename(fn))
     os.remove(file_name) 
-    folder = 'images'
+    folder = 'ss6'
     images = []
     for filename in os.listdir(folder):
         images.append(filename)
@@ -62,22 +63,22 @@ def delete():
 def update():
     fn = request.form["oldfile"]
     file = request.files["file"]
-    file_name = os.path.join('/home/indhu/Desktop/AttendanceSystem/images/', secure_filename(file.filename))
+    file_name = os.path.join('/home/indhu/Desktop/AttendanceSystem/ss6/', secure_filename(file.filename))
     file.save(file_name)
-    fn1 = os.path.join('/home/indhu/Desktop/AttendanceSystem/images/', secure_filename(fn))
+    fn1 = os.path.join('/home/indhu/Desktop/AttendanceSystem/ss6/', secure_filename(fn))
     os.remove(fn1)
-    folder = 'images'
+    folder = 'ss6'
     images = []
     for filename in os.listdir(folder):
         images.append(filename)
     return render_template('admin.html',images=images)
-
+    
 @app.route('/insert', methods=["GET", "POST"])
 def insert():
     file = request.files["file"]
-    file_name = os.path.join('/home/indhu/Desktop/AttendanceSystem/images/', secure_filename(file.filename))
+    file_name = os.path.join('/home/indhu/Desktop/AttendanceSystem/ss6/', secure_filename(file.filename))
     file.save(file_name)
-    folder = 'images'
+    folder = 'ss6'
     images = []
     for filename in os.listdir(folder):
         images.append(filename)
@@ -104,11 +105,11 @@ def admin():
             psw = admin[1]
         print(admin[0], psw)
         if admin[0] == nm and psw == ps:
-            folder = 'students'
+            folder = 'ss6'
             images = []
             for filename in os.listdir(folder):
                 images.append(filename)
-            return render_template('admin.html', images=images)
+            return render_template('admin.html', images=images, flg=True)
     return "Invalid Credentials!"
 
 @app.route("/attendance")
@@ -124,5 +125,5 @@ def face_recognition():
         for line in myDataList:
             entry = line.split(',')
             nameList.append(entry[0])
-    pth = "url_for('static', filename=" + "images/" + nameList[-1] + ".jpg')"
+    pth = "url_for('static', filename=" + "ss6/" + nameList[-1] + ".jpg')"
     return render_template('home.html', name = 'Mr.abcd', flg=flg, status=nameList[-1], pth=pth)
